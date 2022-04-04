880f4e8960770a843b28385d61b6c3272bcc1936
JinwooJung <iod1124@naver.com>
2022-04-04 23:28:51 +0900

    JS 호출스택 예시

--- 1장/functionCall.js
+++ 1장/functionCall.js
@@ -2,13 +2,14 @@ const add = (a,b)=>{ // 함수 선언
     return a+b;
 } 
 
-const add1 = (a,b) => a+b;
+const add1 = (a,b) => a+b;// 선언
 
 
-function calculator (func, a,b){
+function calculator (func, a,b){// 선언
     return func(a,b);
 }
 
+const test = ()=>()=>{} // 고차원함수 
 
 const result = calculator(add1, 3, 5);
 // const result = calculator(add1(3,5), 3, 5); --> add1(3,5) 함수 자리에 함수 호출을 넣는것
